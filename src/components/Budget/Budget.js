91f6afd7dd180c51ab226b76575ed911284e2c43
Rob-Swensen <rg.swensen801@gmail.com>
2020-03-23 20:02:29 -0600

    First Commit

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import Background from './../shared/Background/Background'
-import Chart1 from './../shared/Chart1';
-import Chart2 from './../shared/Chart2';
-import AddPurchase from './../shared/AddPurchase';
-import DisplayPurchases from './../shared/DisplayPurchases';
-import Loading from './../shared/Loading/Loading';
-import Nav from './../shared/Nav';
-import './Budget.css';
-
+import React, { Component } from "react";
+import Background from "./../shared/Background/Background";
+import Chart1 from "./../shared/Chart1";
+import Chart2 from "./../shared/Chart2";
+import AddPurchase from "./../shared/AddPurchase";
+import DisplayPurchases from "./../shared/DisplayPurchases";
+import Loading from "./../shared/Loading/Loading";
+import Nav from "./../shared/Nav";
+import "./Budget.css";
+import { connect } from "react-redux";
+import { requestUserData } from "../../ducks/userReducer";
 
 class Budget extends Component {
+  componentDidMount() {
+    this.props.requestUserData();
+  }
 
   render() {
+    const { loading } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
@@ -29,8 +35,15 @@ class Budget extends Component {
           </div>
         </div>
       </Background>
-    )
+    );
   }
 }
 
-export default Budget;
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps, { requestUserData })(Budget);
